289e5141fc67e88f63aa690be0dfeafb2a3f95b8
Ayush Shrivastava <43237580+Ayush2305@users.noreply.github.com>
2023-03-22 18:41:46 -0400

    Add files via upload
    Formatted buttons added

--- main-tk.py
+++ main-tk.py
@@ -9,7 +9,10 @@ import threading
 import queue
 import tkinter as tk
 from tkinter import ttk
-
+from tkinter import * 
+from tkinter.ttk import *
+from tkinter import filedialog
+import os
 
 
 LARGEFONT =("Verdana", 35)
@@ -139,6 +142,9 @@ LARGEFONT =("Verdana", 35)
 #     root.after(200, update_text)
 # def btn_press(queue):
 #     controller.show_frame(WelcomePage)
+
+
+
 class VFITApp(tk.Tk):
     # __init__ function for class tkinterApp
     def __init__(self, *args, **kwargs):
@@ -148,7 +154,6 @@ class VFITApp(tk.Tk):
         # creating a container
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
@@ -157,13 +162,19 @@ class VFITApp(tk.Tk):
 
         welcome_frame = WelcomePage(container, self)
         setup_frame = SetupPage(container, self)
+        excercise_frame = ExcercisePage(container, self)
+        video_frame = VideoPage(container, self)
 
         self.frames[WelcomePage] = welcome_frame
         self.frames[SetupPage] = setup_frame
+        self.frames[ExcercisePage] = excercise_frame
+        self.frames[VideoPage] = video_frame
 
         setup_frame.grid(row=0, column=0, sticky="nsew")
         welcome_frame.grid(row=0, column=0, sticky="nsew")
-
+        excercise_frame.grid(row=0, column=0, sticky="nsew")
+        video_frame.grid(row=0, column=0, sticky="nsew")
+        
         self.show_frame(WelcomePage)
 
     # to display the current frame passed as
@@ -176,25 +187,72 @@ class VFITApp(tk.Tk):
 class WelcomePage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-
         # WelcomeButton
-        btn = ttk.Button(self, text="Welcome", command=lambda: controller.show_frame(SetupPage))
+        welcomebtn = ttk.Button(self, text="Welcome", command=lambda: controller.show_frame(SetupPage))
         # label = ttk.Label(self, text="Startpage", font=LARGEFONT)
-        btn.place(relx=.5, rely=.5, anchor='center')
+        welcomebtn.place(relx=.5, rely=.5, anchor='center', relheight=0.5, relwidth=0.5)
 
 
 class SetupPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-
+        
         # WelcomeButton
-        btn = ttk.Button(self, text="Setup", command=lambda: controller.show_frame(WelcomePage))
+        setupbtn = ttk.Button(self, text="Mic Test")
         # label = ttk.Label(self, text="Startpage", font=LARGEFONT)
-        btn.place(relx=.5, rely=.5, anchor='center')
+        setupbtn.place(relx=.3, rely=.15, anchor='center', relheight=0.2, relwidth=0.3)
+        
+        micrecbtn = ttk.Button(self, text="Mic Recognition")
+        # label = ttk.Label(self, text="Startpage", font=LARGEFONT)
+        micrecbtn.place(relx=.7, rely=.15, anchor='center', relheight=0.2, relwidth=0.3)
+        slider = ttk.Scale(
+            self,
+            from_=0,
+            to=100,
+            orient='horizontal', 
+        )
+        text=Label(self, text = "Audio")
+        text.config(font =("Courier", 14))
+        text.place(relx=.2, rely=.5, anchor='center', relheight=0.1, relwidth=0.1)
+        slider.get()
+        slider.place(relx=.6, rely=.5, anchor='center', relheight=0.2, relwidth=0.5)
+        readybtn = ttk.Button(self, text="Ready !", command=lambda: controller.show_frame(ExcercisePage))
+        # label = ttk.Label(self, text="Startpage", font=LARGEFONT)
+        readybtn.place(relx=.5, rely=.75, anchor='center', relheight=0.2, relwidth=0.4)
 
+       
+class ExcercisePage(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        bicep = tk.PhotoImage(file='bicep-clipart-11.png')
+        tk.Frame.__init__(self, parent)
+        bicepbtn = ttk.Button(self, text="Bicep", command=lambda: controller.show_frame(VideoPage))
+        # label = ttk.Label(self, text="Startpage", font=LARGEFONT)
+        bicepbtn.place(relx=0.2, rely=0.5, anchor='center', relheight=0.75, relwidth=0.15)
+        
+        lungebtn = ttk.Button(self, text="Lunge", command=lambda: controller.show_frame(VideoPage))
+        # label = ttk.Label(self, text="Startpage", font=LARGEFONT)
+        lungebtn.place(relx=0.4, rely=0.5, anchor='center', relheight=0.75, relwidth=0.15)
+        
+        squatbtn = ttk.Button(self, text="Squat", command=lambda: controller.show_frame(VideoPage))
+        # label = ttk.Label(self, text="Startpage", font=LARGEFONT)
+        squatbtn.place(relx=0.6, rely=0.5, anchor='center', relheight=0.75, relwidth=0.15)
+        
+        jumpingbtn = ttk.Button(self, text="Jumping", command=lambda: controller.show_frame(VideoPage))
+        # label = ttk.Label(self, text="Startpage", font=LARGEFONT)
+        jumpingbtn.place(relx=0.8, rely=0.5, anchor='center', relheight=0.75, relwidth=0.15)
+        
+class VideoPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        # insert video feed and ref video
+        
+        
+        
 
 
 if __name__ == "__main__":
+    
     app = VFITApp()
     app.mainloop()
 
